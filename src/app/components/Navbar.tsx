d6bfc021674ee73d3dc010a6e752ba738fa63983
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-09-27 14:26:21 +0800

    post page done

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -16,11 +16,11 @@ const Navbar: React.FC<Props> = ({ children }) => {
           id="buttons"
           className="flex justify-between w-full px-3 pt-2 mb-2"
         >
-          <HoverBox className="hover:bg-neutral-100 p-2 cursor-pointer rounded">
+          <HoverBox className="hover:bg-neutral-200 p-2 cursor-pointer rounded">
             <Icon icon="iconoir:sidebar-collapse" className="h-5 w-5" />
           </HoverBox>
 
-          <HoverBox className="hover:bg-neutral-100 p-2 cursor-pointer rounded">
+          <HoverBox className="hover:bg-neutral-200 p-2 cursor-pointer rounded">
             <Icon icon="lucide:edit" className="h-5 w-5" />
           </HoverBox>
         </div>
@@ -28,13 +28,13 @@ const Navbar: React.FC<Props> = ({ children }) => {
         {/* THIS CONTAINS YOUR POSTS/MEMOS */}
         <div className="overflow-auto flex-grow mb-3">
           <div id="menu-buttons" className="px-3 mt-2 mb-6">
-            <HoverBox className="hover:bg-neutral-100 p-2 cursor-pointer rounded">
+            <HoverBox className="hover:bg-neutral-200 p-2 cursor-pointer rounded">
               <div className="flex items-center gap-2">
                 <Icon icon="ph:hospital-fill" className="h-5 w-5" />
                 <p className="w-full">Intranet</p>
               </div>
             </HoverBox>
-            <HoverBox className="hover:bg-neutral-100 p-2 cursor-pointer rounded">
+            <HoverBox className="hover:bg-neutral-200 p-2 cursor-pointer rounded">
               <div className="flex items-center gap-2">
                 <Icon
                   icon="fluent:grid-circles-24-regular"
@@ -53,7 +53,7 @@ const Navbar: React.FC<Props> = ({ children }) => {
                     .fill(0)
                     .map((_, index) => (
                       <Link href={`/post/${index}`} key={index}>
-                        <HoverBox className="hover:bg-neutral-100 py-1 px-2 cursor-pointer rounded">
+                        <HoverBox className="hover:bg-neutral-200 py-1 px-2 cursor-pointer rounded">
                           Post {index + 1}
                         </HoverBox>
                       </Link>
@@ -64,12 +64,27 @@ const Navbar: React.FC<Props> = ({ children }) => {
           </div>
         </div>
         <div className="px-3 mb-3">
-          <HoverBox className="hover:bg-neutral-100 p-2 cursor-pointer rounded">
+          <HoverBox className="hover:bg-neutral-200 p-2 cursor-pointer rounded">
             <p>hi</p>
           </HoverBox>
         </div>
       </nav>
-      <div className="max-h-screen overflow-auto w-full">{children}</div>
+      <div className="max-h-screen overflow-auto relative w-full pb-2 px-6">
+        <div
+          className=" sticky w-full flex justify-between pt-3 pb-3 top-0 bg-neutral-200"
+          id="hi"
+        >
+          <p className="text-2xl font-semibold">Intranet</p>
+          <div className="flex items-center gap-3">
+            <button className="rounded-full border border-neutral-800 h-9 flex hover:bg-neutral-300 items-center justify-center gap-1 px-4">
+              <Icon icon="ri:share-2-fill" className="h-5 w-5" />
+              <p>Share</p>
+            </button>
+            <button className="rounded-full h-9 w-9 bg-white"></button>
+          </div>
+        </div>
+        <div>{children}</div>
+      </div>
     </div>
   );
 };
