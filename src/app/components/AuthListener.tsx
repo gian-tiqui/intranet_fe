a813091dc8c122a068eb5874fd3587ccf96b9191
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-09-27 16:53:00 +0800

    jkasndskjnasdjn

--- src/app/components/AuthListener.tsx
+++ src/app/components/AuthListener.tsx
@@ -1,17 +1,27 @@
 "use client";
-import { redirect } from "next/navigation";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import Cookies from "js-cookie";
+import { usePathname, useRouter } from "next/navigation";
 import useNavbarVisibilityStore from "../store/navbarVisibilityStore";
 
 const AuthListener = () => {
   const { setHidden } = useNavbarVisibilityStore();
+  const router = useRouter();
+  const pathname = usePathname();
+
   useEffect(() => {
-    if (!localStorage.getItem("intranet") && !Cookies.get("intranet")) {
-      setHidden(true);
-      redirect("/login");
-    }
-  }, [setHidden]);
+    const checkAuth = () => {
+      if (!localStorage.getItem("intranet") && !Cookies.get("intranet")) {
+        setHidden(true);
+        router.push("/login");
+      } else {
+        setHidden(false);
+      }
+    };
+
+    checkAuth();
+  }, [pathname, router, setHidden]);
+
   return <div></div>;
 };
 
