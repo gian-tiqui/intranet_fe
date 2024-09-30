7239e5091199a9e5272992c803d394d95c8497cc
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-09-30 15:57:48 +0800

    custom error page added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ErrorPage from "./components/ErrorPage";
+
+const Custom404Page = () => {
+  return (
+    <>
+      <ErrorPage />
+    </>
+  );
+};
+
+export default Custom404Page;
