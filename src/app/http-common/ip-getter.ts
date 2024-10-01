4204e678ec4373c331a816b3988b0745538e4479
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-10-01 11:11:16 +0800

    meow

---
+++ src/app/http-common/ip-getter.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const useFetchIP = () => {
+  const [ip, setIp] = useState(null);
+  useEffect(() => {
+    const getIp = async () => {
+      const ipData = await axios.get("https://api64.ipify.org?format=json");
+
+      setIp(ipData.data);
+    };
+
+    getIp();
+  }, []);
+  return ip ? ip : null;
+};
+
+export default useFetchIP;
