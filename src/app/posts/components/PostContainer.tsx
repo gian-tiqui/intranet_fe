a5217f5f7edc8979b07d3cf5aed2321b8a2ca155
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-10-04 16:50:15 +0800

    meow

--- src/app/posts/components/PostContainer.tsx
+++ src/app/posts/components/PostContainer.tsx
@@ -1,6 +1,5 @@
 "use client";
 import usePost from "@/app/custom-hooks/post";
-import { Icon } from "@iconify/react/dist/iconify.js";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { format } from "date-fns";
