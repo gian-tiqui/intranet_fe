657ea8fbada73c82b3e5b5ea851fc0a78796c115
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-10-11 11:25:55 +0800

    login design renovation starts here

--- src/app/login/components/Appbar.tsx
+++ src/app/login/components/Appbar.tsx
@@ -1,18 +1,9 @@
 import ModeToggler from "@/app/components/ModeToggler";
-import { Icon } from "@iconify/react/dist/iconify.js";
 import React from "react";
 
 const Appbar = () => {
   return (
-    <div className="gap-10 md:w-[500px] h-14 bg-white dark:bg-neutral-900 shadow border dark:border-neutral-800 rounded-2xl mt-10 md:mt-0 mb-8 flex justify-between px-5 items-center">
-      <div className="">
-        <Icon icon={"ph:hospital"} className="h-8 w-8" />
-      </div>
-
-      <div className="w-full">
-        <h1 className="text-center font-mono font-bold text-2xl">INTRANET</h1>
-      </div>
-
+    <div className="w-full justify-end flex p-10">
       <div className="">
         <ModeToggler />
       </div>
