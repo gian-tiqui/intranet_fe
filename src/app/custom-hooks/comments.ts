332a8130fbd721e4b98b9f43fe7d34074a3d0478
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-10-11 09:29:52 +0800

    added api key

--- src/app/custom-hooks/comments.ts
+++ src/app/custom-hooks/comments.ts
@@ -17,7 +17,6 @@ const useComments = () => {
         });
 
         if (response.status === 200) {
-          console.log(response.data);
           setComments(response.data);
         } else {
           console.log("There was a problem in fetching the comments");
