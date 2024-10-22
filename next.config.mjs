3a36736817d13631f351bc9740cb72c0050896bf
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-10-22 09:03:11 +0800

    image test

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,11 @@ const nextConfig = {
         port: "8080",
         pathname: "/uploads/post/**",
       },
+      {
+        protocol: "https",
+        hostname: "intranet-api-4yk4.onrender.com",
+        pathname: "/uploads/post/**",
+      },
     ],
   },
 };
