e072af18fcb244b68b75abe215bcbbdd00c79ef0
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-10-31 14:41:34 +0800

    auto logout done

--- src/app/components/AuthListener.tsx
+++ src/app/components/AuthListener.tsx
@@ -4,10 +4,15 @@ import Cookies from "js-cookie";
 import { usePathname, useRouter } from "next/navigation";
 import useNavbarVisibilityStore from "../store/navbarVisibilityStore";
 import { INTRANET } from "../bindings/binding";
+import { jwtDecode } from "jwt-decode";
+
+interface DecodedToken {
+  exp: number;
+}
 
 /*
  *  This component will be used to check if the user on the initial load is logged in
- *  by checking there refresh and access tokens which are stored in the brower's
+ *  by checking their refresh and access tokens which are stored in the browser's
  *  cookies and local storage.
  */
 
@@ -18,11 +23,26 @@ const AuthListener = () => {
 
   useEffect(() => {
     const checkAuth = () => {
-      if (!localStorage.getItem(INTRANET) && !Cookies.get(INTRANET)) {
+      const accessToken = localStorage.getItem(INTRANET);
+      const refreshToken = Cookies.get(INTRANET);
+
+      if (!accessToken && !refreshToken) {
         setHidden(false);
         router.push("/welcome");
       } else {
-        setHidden(true);
+        if (refreshToken) {
+          const decoded: DecodedToken = jwtDecode(refreshToken);
+          const currentTime = Math.floor(Date.now() / 1000);
+
+          if (decoded.exp < currentTime) {
+            setHidden(false);
+            router.push("/welcome");
+          } else {
+            setHidden(true);
+          }
+        } else {
+          setHidden(true);
+        }
       }
     };
 
