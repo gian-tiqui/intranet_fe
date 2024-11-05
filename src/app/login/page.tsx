38c8e128f56902c74d9ef939ce954d98de992d63
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-11-05 14:15:19 +0800

    initial login page design added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,19 +3,14 @@ import Form from "./components/Form";
 import LoginTemplate from "./template/LoginTemplate";
 import facade from "../assets/Hospital Facade Edited 2.png";
 import Image from "next/image";
-import ModeToggler from "../components/ModeToggler";
 
 const Login = () => {
   return (
     <div className="relative">
       <LoginTemplate>
-        <div className="grid md:grid-cols-2">
-          <div className="relative">
-            <Image src={facade} alt="Westlake Facade" fill />
-            <div className="absolute">
-              <ModeToggler />
-            </div>
-          </div>
+        <div className="grid place-content-center">
+          <Image src={facade} alt="Westlake Facade" fill />
+          <div className="absolute w-screen bg-gradient-to-t from-blue-600/70 to-blue-500/70 dark:from-blue-900/70 dark:to-blue-800/70 h-screen"></div>
 
           <Form />
         </div>
