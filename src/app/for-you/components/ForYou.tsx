8957e5cbd21a37f5f3093fade48b716e9bf5776c
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-11-28 11:45:49 +0800

    yey

--- src/app/for-you/components/ForYou.tsx
+++ src/app/for-you/components/ForYou.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { API_BASE } from "@/app/bindings/binding";
 import NoPosts from "@/app/bulletin/components/NoPosts";
 import Shortcuts from "@/app/bulletin/components/Shortcuts";
-import { fetchPostsByLevel } from "@/app/functions/functions";
+import { decodeUserData } from "@/app/functions/functions";
+import apiClient from "@/app/http-common/apiUrl";
 import PostContainer from "@/app/posts/components/PostContainer";
 import PostSkeleton from "@/app/posts/components/PostSkeleton";
-import { useQuery } from "@tanstack/react-query";
+import { Post, RetPost } from "@/app/types/types";
+import { AxiosResponse } from "axios";
 import React, { useEffect, useState } from "react";
 
 const ForYou = () => {
@@ -14,24 +17,35 @@ const ForYou = () => {
     min: 0,
     max: 2,
   });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [totalPosts, setTotalPosts] = useState<number>(0);
-
-  const { data, isError, isLoading, error } = useQuery({
-    queryKey: ["level_posts", direction, minMax],
-    queryFn: fetchPostsByLevel,
-    staleTime: 1000 * 60 * 5,
-    refetchOnWindowFocus: false,
-  });
-
-  const isLastPage = minMax.max >= totalPosts;
+  const [data, setData] = useState<Post[]>([]);
 
   useEffect(() => {
-    if (data) {
-      setTotalPosts(data.count);
-    }
-  }, [data]);
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const response: AxiosResponse<RetPost> = await apiClient.get(
+          `${API_BASE}/post/level/${decodeUserData()?.lid}?deptId=${
+            decodeUserData()?.deptId
+          }&offset=${minMax.min}&limit=${minMax.max}&direction=${direction}`
+        );
 
-  if (isError) console.error(error);
+        console.log(response.data.count);
+
+        setTotalPosts(response.data.count);
+        setData(response.data.posts);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [minMax, direction]);
+
+  const isLastPage = minMax.max >= totalPosts;
 
   return (
     <div>
@@ -39,7 +53,7 @@ const ForYou = () => {
         <PostSkeleton />
       ) : (
         <>
-          {data?.posts && data.posts.length > 0 ? (
+          {data && data.length > 0 ? (
             <>
               <Shortcuts
                 min={minMax.min}
@@ -50,7 +64,7 @@ const ForYou = () => {
                 isLastPage={isLastPage}
               />
 
-              {data.posts.map((post) => (
+              {data.map((post) => (
                 <PostContainer
                   id={post.pid}
                   key={post.pid}
