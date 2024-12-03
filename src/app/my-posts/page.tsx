0adfb5d0e008fe6d6ef2b6b1a7d39b0287386548
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-12-03 10:22:57 +0800

    pls

--- src/app/my-posts/page.tsx
+++ src/app/my-posts/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import MyPosts from "./components/MyPosts";
 import AuthListener from "../components/AuthListener";
+import dynamic from "next/dynamic";
+
+const MyPosts = dynamic(() => import("./components/MyPosts"), { ssr: false });
 
 const MyPostsPage = () => {
   return (
