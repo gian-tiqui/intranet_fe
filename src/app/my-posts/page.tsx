43371dba80bdd4a817d99f2dc646e48cf88b2f01
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-12-03 10:28:41 +0800

    pls

--- src/app/my-posts/page.tsx
+++ src/app/my-posts/page.tsx
@@ -4,10 +4,10 @@ import AuthListener from "../components/AuthListener";
 
 const MyPostsPage = () => {
   return (
-    <>
+    <div>
       <AuthListener />
       <MyPosts />
-    </>
+    </div>
   );
 };
 
