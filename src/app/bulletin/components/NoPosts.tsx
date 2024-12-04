302d567602ca28a42494b36218d64b6d43557e84
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-12-04 15:35:52 +0800

    modified no posts display

--- src/app/bulletin/components/NoPosts.tsx
+++ src/app/bulletin/components/NoPosts.tsx
@@ -2,10 +2,8 @@ import React from "react";
 
 const NoPosts = () => {
   return (
-    <div className="h-52 w-full grid place-content-center">
-      <p className="text-lg font-semibold px-20 py-7 shadow bg-white dark:bg-neutral-900 rounded-lg">
-        No posts yet.
-      </p>
+    <div className="pt-10 h-52 w-full">
+      <p className="">No posts yet.</p>
     </div>
   );
 };
