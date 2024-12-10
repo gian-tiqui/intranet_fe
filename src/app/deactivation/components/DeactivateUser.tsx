95e46354dd0e70a1d7400899f4a7964d7c1319ca
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-12-10 16:55:07 +0800

    deactivation password input and design left

--- src/app/deactivation/components/DeactivateUser.tsx
+++ src/app/deactivation/components/DeactivateUser.tsx
@@ -1,7 +1,10 @@
 "use client";
+import { API_BASE } from "@/app/bindings/binding";
+import { decodeUserData } from "@/app/functions/functions";
+import apiClient from "@/app/http-common/apiUrl";
 import useUserStore from "@/app/store/userStore";
 import { User } from "@/app/types/types";
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   user: User;
@@ -9,6 +12,21 @@ interface Props {
 
 const DeactivateUser: React.FC<Props> = ({ user }) => {
   const { setUser } = useUserStore();
+  const [password] = useState<string>("password1");
+
+  const handleClick = async () => {
+    const deactivatorId = decodeUserData()?.sub;
+
+    try {
+      const response = await apiClient.post(
+        `${API_BASE}/users/deactivate?password=${password}&deactivatorId=${deactivatorId}&employeeId=${user.employeeId}`
+      );
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div
@@ -17,7 +35,7 @@ const DeactivateUser: React.FC<Props> = ({ user }) => {
     >
       <div className="w-80 bg-white" onClick={(e) => e.stopPropagation()}>
         {" "}
-        {user.firstName}
+        deactivate {user.firstName}?<button onClick={handleClick}>ok</button>
       </div>
     </div>
   );
