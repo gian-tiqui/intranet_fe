d2f824e0896a5d2484daa7d5e2efadc64e9446b0
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-12-15 14:19:43 +0800

    temporary fix to react-toastify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/ReactToastify.css";
+import "react-toastify/ReactToastify.min.css";
 import WelcomeSidebar from "./welcome/components/WelcomeSidebar";
 import Divider from "./components/Divider";
 
