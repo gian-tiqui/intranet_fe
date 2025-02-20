5a0ca95d8fe2cd5cfc100039f7b919f7d360b52b
gian-tiqui <gian.tiqui.dev@gmail.com>
2025-02-20 11:19:43 +0800

    enhanced forgot password

---
+++ src/app/utils/misc/questions.ts
@@ -0,0 +1,19 @@
+const questions: string[] = [
+  "Select a question",
+  "What is your first pet's name?",
+  "What is the name of your first school?",
+  "In what city were you born?",
+  "What is your mother's maiden name?",
+  "What was the make and model of your first car?",
+  "What is the name of your favorite childhood friend?",
+  "What is the name of the street you grew up on?",
+  "What was your childhood nickname?",
+  "What is your favorite book?",
+  "What is your favorite movie?",
+  "What was the name of your first teacher?",
+  "What is the name of your first employer?",
+  "What is the name of your first stuffed animal?",
+  "What is the middle name of your oldest sibling?",
+];
+
+export { questions };
