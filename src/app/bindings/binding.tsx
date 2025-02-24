3bb490d9f4f2eb6b450c15f538fa502f54feb0d2
gian-tiqui <gian.tiqui.dev@gmail.com>
2025-02-24 15:54:03 +0800

    added intranet in bindings

--- src/app/bindings/binding.tsx
+++ src/app/bindings/binding.tsx
@@ -1,4 +1,4 @@
-export const INTRANET = process.env.NEXT_PUBLIC_INTRANET;
+export const INTRANET = process.env.NEXT_PUBLIC_INTRANET || "intranet";
 export const API_BASE = process.env.NEXT_PUBLIC_API_URL;
 export const PROJECT_VERSION_KEY = "epv";
 export const PROJECT_VERSION = process.env.NEXT_PUBLIC_PROJECT_VERSION;
