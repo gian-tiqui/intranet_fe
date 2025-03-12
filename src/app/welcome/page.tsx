b7a78ad23e38b7af4372d5ecbe1451fa58f37ecb
gian-tiqui <gian.tiqui.dev@gmail.com>
2025-03-12 09:47:49 +0800

    changed the design

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,12 +1,28 @@
 import React from "react";
+import Image from "next/image";
 import WelcomeNavbar from "./components/WelcomeNavbar";
 import Hero from "./components/Hero";
+import bg from "../assets/Hospital Facade Edited 2.png";
 
 const WelcomePage = () => {
   return (
-    <div className="bg-neutral-50 dark:bg-neutral-950">
-      <WelcomeNavbar />
-      <Hero />
+    <div className="relative h-screen">
+      <div className="absolute inset-0 z-0 opacity-60">
+        <Image
+          src={bg.src}
+          alt="Hospital Facade"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="0% 58%"
+          quality={100}
+          priority
+        />
+      </div>
+
+      <div className="relative z-10">
+        <WelcomeNavbar />
+        <Hero />
+      </div>
     </div>
   );
 };
