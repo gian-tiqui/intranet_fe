fab303a75796b8714240b19c4b3dcd2abea1fba0
gian-tiqui <gian.tiqui.dev@gmail.com>
2025-05-15 14:04:18 +0800

    added drafts service

--- src/app/drafts/components/Drafts.tsx
+++ src/app/drafts/components/Drafts.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Drafts = () => {
-  return <div className="w-full h-screen"></div>;
+  return (
+    <div className="w-full h-[86vh] overflow-y-auto pt-4">
+      <h4 className="font-medium text-blue-600 text-lg">Drafts</h4>
+    </div>
+  );
 };
 
 export default Drafts;
