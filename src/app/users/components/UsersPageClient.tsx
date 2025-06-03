d2b9a9667d6de71c8cc07d5eaa890d7222458caa
gian-tiqui <gian.tiqui.dev@gmail.com>
2025-06-03 16:29:44 +0800

    m

--- src/app/users/components/UsersPageClient.tsx
+++ src/app/users/components/UsersPageClient.tsx
@@ -1,7 +1,10 @@
 "use client";
+import AuthListener from "@/app/components/AuthListener";
 import { Query } from "@/app/types/types";
 import { findUsers } from "@/app/utils/service/userService";
 import { useQuery } from "@tanstack/react-query";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
 import React, { useState } from "react";
 
 const UsersPageClient = () => {
@@ -14,7 +17,13 @@ const UsersPageClient = () => {
 
   return (
     <div className="w-full h-[86vh] overflow-y-auto">
-      {JSON.stringify(data?.data.users)}
+      <AuthListener />
+      <DataTable value={data?.data.users} size="small" className="text-sm">
+        <Column field="firstName" header="First name" />
+        <Column field="middleName" header="Middle Name" />
+        <Column field="lastName" header="Last Name" />
+        <Column />
+      </DataTable>
     </div>
   );
 };
