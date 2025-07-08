b1b56f71294bf95c340e9224752d893f4872952a
gian-tiqui <gian.tiqui.dev@gmail.com>
2025-07-08 16:02:05 +0800

    m

--- src/app/store/userId.ts
+++ src/app/store/userId.ts
@@ -2,12 +2,12 @@ import { create } from "zustand";
 
 interface State {
   selectedId: number | undefined;
-  setSelectedId: (selectedId: number) => void;
+  setSelectedId: (selectedId: number | undefined) => void;
 }
 
 const userIdStore = create<State>((set) => ({
   selectedId: undefined,
-  setSelectedId: (selectedId: number) => set({ selectedId }),
+  setSelectedId: (selectedId: number | undefined) => set({ selectedId }),
 }));
 
 export default userIdStore;
